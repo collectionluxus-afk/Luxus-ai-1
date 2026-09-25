c9665c20e95352e4d37b15bbcf26ff475c5099cf
collectionluxus-afk <collectionluxus@gmail.com>
2026-09-25 20:19:29 +0530

    Fix JVM target compatibility for Kotlin build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
 android {
     namespace="com.collectionluxus.personalhisab"
     compileSdk=35
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
     defaultConfig {
         applicationId="com.collectionluxus.personalhisab"
         minSdk=24
